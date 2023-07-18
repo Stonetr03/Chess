564c7f5aa1ddca1edfe8f950d7a006ad4d565d31
Stonetr03 <67619579+Stonetr03@users.noreply.github.com>
2023-07-17 23:05:12 -0700

    Signal
    Signed-off-by: Stonetr03 <67619579+Stonetr03@users.noreply.github.com>

--- src/server/game/init.lua
+++ src/server/game/init.lua
@@ -5,18 +5,22 @@ local PrintBoard = require(script:WaitForChild("PrintBoard"))
 local HttpService = game:GetService("HttpService")
 local Moves = require(script:WaitForChild("Moves"))
 local PlayMove = require(script:WaitForChild("PlayMove"))
+local Signal = require(game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Signal"))
 
 local legalPromote = {"r","n","b","q","","nil"}
 
 local Module = {
-    Games = {}
+    Games = {};
+    Signals = {};
 }
 
 function Module:NewGame()
     local Hash = HttpService:GenerateGUID(true)
     Module.Games[Hash] = NewGame:New(Hash)
     PrintBoard(Module.Games[Hash])
-    return Hash
+    local GameSignal = Signal.new()
+    Module.Signals[Hash] = GameSignal;
+    return Hash,GameSignal
 end
 
 function Module:GetLegalMoves(Hash,Square)
@@ -33,6 +37,7 @@ function Module:Playmove(Hash,Player,Square,Move,Promote)
         local Moved,New,UpdateMoves = PlayMove:Move(Board,Player,Square,Move,Promote)
         if Moved == true then
             Module.Games[Hash] = New
+            Module.Signals[Hash]:Fire(UpdateMoves,New)
         end
 
         PrintBoard(Module.Games[Hash])
@@ -56,6 +61,7 @@ function Module:Draw(Hash,Player,v)
         Board.Turn = ""
         Board.Status = "Draw;Agreement"
         Board.PGN = Board.PGN .. " 1/2-1/2"
+        Module.Signals[Hash]:Fire({},Board)
     end
 end
 
@@ -66,10 +72,12 @@ function Module:Resign(Hash,Player)
             Board.Turn = ""
             Board.Status = "Resign;b"
             Board.PGN = Board.PGN .. " 0-1"
+            Module.Signals[Hash]:Fire({},Board)
         elseif Board.Black == Player then
             Board.Turn = ""
             Board.Status = "Resign;w"
             Board.PGN = Board.PGN .. " 1-0"
+            Module.Signals[Hash]:Fire({},Board)
         end
     end
 end
